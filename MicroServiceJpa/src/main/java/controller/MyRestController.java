963953e6efa2ac4e332576fbf6e764664062cb49
praveenqie <50437963+praveenqie@users.noreply.github.com>
2020-07-17 23:52:02 +0530

    updated

--- MicroServiceJpa/src/main/java/controller/MyRestController.java
+++ MicroServiceJpa/src/main/java/controller/MyRestController.java
@@ -1,8 +1,19 @@
 package controller;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
-@Repository
+
+
+@RestController
+@RequestMapping(value = "/api/v1",produces = MediaType.APPLICATION_JSON_VALUE)
 public class MyRestController {
-	
+
+	@RequestMapping(value = "/hello",method = RequestMethod.GET)
+	public void myApiCall() {
+		System.out.println("First API");
+	}
+
 }
